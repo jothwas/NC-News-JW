eeb3d221457486c2fee136e42405c26f9e56723b
joshua-walters <87895876+jothwas@users.noreply.github.com>
2022-03-08 11:45:26 +0000

    list of all articles

--- src/components/Articles/ArticlesList.jsx
+++ src/components/Articles/ArticlesList.jsx
@@ -1,6 +1,27 @@
 import { AddCircleOutline } from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import * as api from "../../utils/api.js";
+import ArticleCard from "./ArticleCard.jsx";
 
 const ArticlesList = () => {
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    api.fetchAllArticles().then((apiArticles) => {
+      setArticles(apiArticles);
+    });
+    setIsLoading(false);
+  }, []);
+
+  if (isLoading)
+    return (
+      <div>
+        <h2>loading...</h2>
+      </div>
+    );
+
   return (
     <div>
       <header className="article-query-container">
@@ -12,6 +33,11 @@ const ArticlesList = () => {
           create new article
         </h5>
       </header>
+      <main className="article-list-layout">
+        {articles.map((article) => {
+          return <ArticleCard article={article} key={article.article_id} />;
+        })}
+      </main>
     </div>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,36 +3,31 @@ import { AccountCircleSharp, CellTowerOutlined } from "@mui/icons-material";
 
 const Header = () => {
   return (
-    <Navbar variant="dark" className="color-nav">
+    <Navbar collapseOnSelect expand="lg" variant="dark" className="color-nav">
       <Container>
         <Navbar.Brand href="/">
           <CellTowerOutlined className="nav-icon-margin" />
-          readdit: a nc news app
+          // readdit: a nc news app
         </Navbar.Brand>
-        <Nav className="m-auto">
-          <Nav.Link href="#home">
-            <span className="nav-menuText-colour">all articles</span>
-          </Nav.Link>
-          <Nav.Link href="#features">
-            <span className="nav-menuText-colour">coding</span>
-          </Nav.Link>
-          <Nav.Link href="#pricing">
-            <span className="nav-menuText-colour">football</span>
-          </Nav.Link>
-          <Nav.Link href="#pricing">
-            <span className="nav-menuText-colour">cooking</span>
-          </Nav.Link>
-          <NavDropdown title="More Topics" id="navbarScrollingDropdown">
-            <NavDropdown.Item href="#action3">Topic 1</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Topic 2</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Nav>
-          <Navbar.Brand href="#home">
-            <AccountCircleSharp className="nav-icon-margin" />
-            login
-          </Navbar.Brand>
-        </Nav>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="all">all articles</Nav.Link>
+            <Nav.Link href="coding">coding</Nav.Link>
+            <Nav.Link href="football">football</Nav.Link>
+            <Nav.Link href="cooking">cooking</Nav.Link>
+            <NavDropdown
+              title="more"
+              id="collasible-nav-dropdown"
+            ></NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link eventKey={2} href="#memes">
+              <AccountCircleSharp className="nav-icon-margin" />
+              // login
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
