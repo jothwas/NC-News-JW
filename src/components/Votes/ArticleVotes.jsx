071a9d9b72b2754666b7b20653b7fda414e2f929
joshua-walters <87895876+jothwas@users.noreply.github.com>
2022-03-09 22:35:14 +0000

    add vote disabler after click and colour highlight for vote

--- src/components/Votes/ArticleVotes.jsx
+++ src/components/Votes/ArticleVotes.jsx
@@ -5,24 +5,36 @@ import {
   ArrowUpwardSharp,
   ModeCommentOutlined,
 } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 
 const ArticleVotes = ({ article_id, votes, comment_count }) => {
   const [articleVotes, setArticleVotes] = useState(votes);
+  const [voteCheck, setVoteCheck] = useState(0);
 
   const voteAction = (votecrement) => {
-    setArticleVotes((currentVoteCount) => currentVoteCount + votecrement);
     api.patchArticleVotes(article_id, votecrement);
+    setArticleVotes((currentVoteCount) => currentVoteCount + votecrement);
+    setVoteCheck((zero) => zero + votecrement);
   };
 
   return (
     <div className="article-card-footer">
       <div className="article-card-vote-count">
-        <ArrowUpwardSharp className="up-arrow" onClick={() => voteAction(1)} />
+        <IconButton onClick={() => voteAction(1)} disabled={voteCheck === 1}>
+          <ArrowUpwardSharp
+            className={`up-arrow ${
+              voteCheck === 1 ? `orange-highlight` : null
+            }`}
+          />
+        </IconButton>
         {articleVotes}
-        <ArrowDownwardSharp
-          className="down-arrow"
-          onClick={() => voteAction(-1)}
-        />
+        <IconButton onClick={() => voteAction(-1)} disabled={voteCheck === -1}>
+          <ArrowDownwardSharp
+            className={`down-arrow ${
+              voteCheck === -1 ? `orange-highlight` : null
+            }`}
+          />
+        </IconButton>
       </div>
       <div className="article-comments">
         <ModeCommentOutlined className="comment-icon" />
--- src/components/Votes/CommentVotes.jsx
+++ src/components/Votes/CommentVotes.jsx
@@ -1,24 +1,36 @@
 import * as api from "../../utils/api.js";
 import { useState } from "react";
 import { ArrowDownwardSharp, ArrowUpwardSharp } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 
 const CommentVotes = ({ comment_id, votes }) => {
   const [commentVotes, setCommentVotes] = useState(votes);
+  const [voteCheck, setVoteCheck] = useState(0);
 
   const voteAction = (votecrement) => {
-    setCommentVotes((currentVoteCount) => currentVoteCount + votecrement);
     api.patchCommentVotes(comment_id, votecrement);
+    setCommentVotes((currentVoteCount) => currentVoteCount + votecrement);
+    setVoteCheck((zero) => zero + votecrement);
   };
 
   return (
     <div className="comment-card-footer">
       <div className="comment-card-vote-count">
-        <ArrowUpwardSharp className="up-arrow" onClick={() => voteAction(1)} />
+        <IconButton onClick={() => voteAction(1)} disabled={voteCheck === 1}>
+          <ArrowUpwardSharp
+            className={`up-arrow ${
+              voteCheck === 1 ? `orange-highlight` : null
+            }`}
+          />
+        </IconButton>
         {commentVotes}
-        <ArrowDownwardSharp
-          className="down-arrow"
-          onClick={() => voteAction(-1)}
-        />
+        <IconButton onClick={() => voteAction(-1)} disabled={voteCheck === -1}>
+          <ArrowDownwardSharp
+            className={`down-arrow ${
+              voteCheck === -1 ? `orange-highlight` : null
+            }`}
+          />
+        </IconButton>
       </div>
     </div>
   );
