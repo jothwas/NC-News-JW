caa100e7d2e687b786b391f85f0d5c7a17c98405
joshua-walters <87895876+jothwas@users.noreply.github.com>
2022-03-10 14:44:59 +0000

    add comment posting functionality

---
+++ src/components/Comments/AddComment.jsx
@@ -0,0 +1,58 @@
+import { useContext, useState } from "react";
+import { Form } from "react-bootstrap";
+import { UserContext } from "../../contexts/UserContext";
+import * as api from "../../utils/api.js";
+
+const AddComment = ({ article_id, setComments }) => {
+  const { loggedInUser } = useContext(UserContext);
+  const [newComment, setNewComment] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const newCommentDetails = {
+      author: loggedInUser.username,
+      body: newComment,
+    };
+
+    setComments((currentComments) => {
+      return [newCommentDetails, ...currentComments];
+    });
+
+    api
+      .postComment(article_id, loggedInUser.username, newComment)
+      .then(() => {
+        setNewComment("");
+      })
+      .catch(() => {
+        setComments((currentComments) => {
+          return currentComments.slice(1);
+        });
+        alert("Error: please submit your comment again");
+      });
+  };
+
+  return (
+    <div className="comment-form-container">
+      <Form onSubmit={handleSubmit}>
+        <Form.Control
+          type="text"
+          placeholder="leave a comment"
+          name="commentInput"
+          value={newComment}
+          onChange={(e) => setNewComment(e.target.value)}
+          required
+          style={{ height: "60px", borderStyle: "solid" }}
+        />
+        <button
+          className="article-card-read-button article-card-read-button1 article-card-post-comment"
+          type="submit"
+        >
+          post your comment
+        </button>
+      </Form>
+    </div>
+  );
+};
+
+export default AddComment;
